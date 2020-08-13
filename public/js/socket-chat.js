0b32fde120618970c1b46f5cd9242ab80c722941
Cristian <presenciapublicitaria@gmail.com>
2020-08-12 19:22:36 -0500

    chat finalizado

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -12,12 +12,16 @@ var usuario = {
     sala: params.get('sala')
 };
 
+
+
 socket.on('connect', function() {
     console.log('Conectado al servidor');
 
     socket.emit('entrarChat', usuario, function(resp) {
-        console.log('Usuarios conectados', resp);
+        // console.log('Usuarios conectados', 
+        renderizarUsuarios(resp);
     });
+
 });
 
 // escuchar
@@ -30,7 +34,7 @@ socket.on('disconnect', function() {
 
 // Enviar información
 // socket.emit('crearMensaje', {
-//     usuario: 'Fernando',
+//     nombre: 'Fernando',
 //     mensaje: 'Hola Mundo'
 // }, function(resp) {
 //     console.log('respuesta server: ', resp);
@@ -38,19 +42,21 @@ socket.on('disconnect', function() {
 
 // Escuchar información
 socket.on('crearMensaje', function(mensaje) {
-
-    console.log('Servidor:', mensaje);
-
+    // console.log('Servidor:', mensaje);
+    renderizarMensajes(mensaje, false);
+    scrollBottom();
 });
 
-// Escuchar cambios de usuarios / ingreso y salida
+// Escuchar cambios de usuarios
+// cuando un usuario entra o sale del chat
 socket.on('listaPersona', function(personas) {
-
-    console.log(personas);
-
+    // console.log(personas);
+    renderizarUsuarios(personas);
 });
 
-// Mensajes Privados
+// Mensajes privados
 socket.on('mensajePrivado', function(mensaje) {
-    console.log('Mensaje Privado: ', mensaje);
+
+    console.log('Mensaje Privado:', mensaje);
+
 });
